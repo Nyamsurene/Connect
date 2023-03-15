7b7a2c6524fda3592c56ebb9e9f7f01d89821cda
teacher <nyamsuren@msue.edu.mn>
2023-03-15 13:12:46 +0800

    anhnii oorchlolt

---
+++ index.php
@@ -0,0 +1,19 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+<div class="container-fluid p-5 bg-primary text-white text-center">
+  <h1>My First Bootstrap Page</h1>
+  <p>Цонхыг томруулж жижигрүүлж үр дүнг хар!</p>
+</div>
+
+
+<div class="container border">
+   <p> .container класс дотор байна.</p>
+  <p>.container класс тогтмол өргөнтэй. Үүний өргөн цонх өөрчлөгдсөн ч өөрчлөгдөхгүй юм.</p>
+</div>
+<div class="container-fluid">
+    <p>T.container-fluid класс энд харагдаж байна.</p>
+  <p>.container-fluid класс нь ямар ч хэмжээтэй цонх дотор бүтэн дүүрч харагдана.</p>
+</div>
